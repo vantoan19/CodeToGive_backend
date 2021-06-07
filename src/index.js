568e893ee7357f7bff3beba28059abca8f3fa242
vantoan19 <thetopoftheworld19@gmai.com>
2021-06-07 14:08:41 +0200

    afdsaf

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+})
+
 app.use(userRouter);
 app.use(guessPicRouter);
 
